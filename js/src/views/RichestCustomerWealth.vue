69da24d0a3640bcf9be967bf58ee977498575130
Vyacheslav <gracerpro@yandex.ru>
2026-02-18 22:13:43 +0300

    1672. Richest Customer Wealth

---
+++ js/src/views/RichestCustomerWealth.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const accounts = [
+  [1, 2, 3],
+  [4, 5, 6],
+]
+
+console.log(maximumWealth(accounts))
+
+function maximumWealth(accounts: number[][]): number {
+  let maxWealth = 0
+  const accountsLength = accounts.length
+
+  for (let i = 0; i < accountsLength; ++i) {
+    const accountWealths = accounts[i]!
+
+    const accountWealthsLength = accountWealths.length
+    let sum = 0
+    for (let j = 0; j < accountWealthsLength; ++j) {
+      sum += accountWealths[j]!
+    }
+    if (sum > maxWealth) {
+      maxWealth = sum
+    }
+  }
+
+  return maxWealth
+}
+</script>
