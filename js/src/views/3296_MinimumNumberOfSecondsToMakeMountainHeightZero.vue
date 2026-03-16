e54150dffc60d07c2fe6c2e5059d1c152443baed
Vyacheslav <gracerpro@yandex.ru>
2026-03-16 23:37:37 +0300

    Prepare to binary search

--- js/src/views/3296_MinimumNumberOfSecondsToMakeMountainHeightZero.vue
+++ js/src/views/3296_MinimumNumberOfSecondsToMakeMountainHeightZero.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-//console.log(minNumberOfSeconds(10, [3,2,2,4]))
-console.log(minNumberOfSeconds(5, [1]))
+console.log(minNumberOfSeconds(10, [3,2,2,4]))
+//console.log(minNumberOfSeconds(5, [1]))
 
 function minNumberOfSeconds(mountainHeight: number, workerTimes: number[]): number {
   if (workerTimes.length === 0) {
@@ -9,39 +9,21 @@ function minNumberOfSeconds(mountainHeight: number, workerTimes: number[]): numb
 
   console.log("mountainHeight", mountainHeight)
 
-  let iterHeight = mountainHeight
-  const seconds = new Array<number>()
-  let i = 0
+  const maxWorkTime = workerTimes.reduce((prev, value) => Math.max(prev, value), 0)
 
-  while (iterHeight > 0) {
-    let reduceHeight = i + 1
+  console.log('maxWorkTime', maxWorkTime)
 
-    if (reduceHeight > workerTimes.length) {
-      reduceHeight = workerTimes.length
-      i = workerTimes.length - 1
-    }
+  let lower = 1
+  let upper = (maxWorkTime * mountainHeight * (mountainHeight + 1)) / 2
+  let result = 0
 
-    iterHeight -= reduceHeight
+  while (lower <= upper) {
 
-    console.log(iterHeight, reduceHeight)
-
-    if (iterHeight < 0) {
-        reduceHeight += iterHeight
-        iterHeight = 0
-    }
-
-    let iterSeconds = 0
-    for (let j = 1; j <= reduceHeight; ++j) {
-        iterSeconds += workerTimes[i]! * j
-    }
-    seconds.push(iterSeconds)
-
-    ++i
+    lower = lower + 1
+    upper = upper - 1
   }
 
-  console.log(seconds)
-
-  return seconds.reduce((a, b) => Math.max(a, b), -Infinity)
+  return result
 };
 </script>
 
