a5e673a8472f56470ee7c91e32a0091ee7d6957c
Vyacheslav <gracerpro@yandex.ru>
2026-03-25 17:18:43 +0300

    1295. Find Numbers with Even Number of Digits

---
+++ js/src/views/1295_FindNumbersWithEvenNumberOfDigits.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+console.log(findNumbers([1, 11, 239, 49976]))
+
+function findNumbers(nums: number[]): number {
+    let result = 0
+    const length = nums.length
+
+    for (let i = 0; i < length; ++i) {
+       /* const s = nums[i].toString()
+
+        if (s.length % 2 === 0) {
+            ++result
+        }*/
+        let number = nums[i]!
+        let count = 0
+        while (number > 0) {
+            ++count
+            number = Math.floor(number / 10)
+        }
+        if (count % 2 === 0) {
+            ++result
+        }
+    }
+
+    return result
+};
+</script>
